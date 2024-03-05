a01f55de9db7dff49af3f9204c189dc05d5fbbe5
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-03-05 17:08:50 +0300

    Add lazy.nvim plugin manager

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -4,3 +4,45 @@ vim.g.maplocalleader = ' '
 require("config.options")
 require("config.keymappings")
 
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {
+    defaults = {
+        lazy = true,
+    },
+    performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      paths = {}, -- add any custom paths here that you want to includes in the rtp
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrw",
+        "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
+      },
+    },
+  },
+}
+
+require("lazy").setup("michael.plugins", opts)
