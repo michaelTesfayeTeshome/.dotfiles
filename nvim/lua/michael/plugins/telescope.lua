75fd0a31d7c1a9bfdeb3e5d0f1c9fd39331c3ec4
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-03-05 18:00:02 +0300

    Add telescope fuzzy finder

---
+++ nvim/lua/michael/plugins/telescope.lua
@@ -0,0 +1,66 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',     -- run make command after the plugin installed
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-tree/nvim-web-devicons' }
+    },
+    config = function()
+      require('telescope').setup({
+        defaults = {
+            mappings = {
+                i = {
+                    ["<C-j>"] = "move_selection_next",
+                    ["<C-k>"] = "move_selection_previous",
+                    ["<C-u>"] = false   -- clear out whats typed on the search prompt
+                }
+            },
+            prompt_prefix = '󰍉  ',
+            selection_caret = '  ',
+            layout_config = {
+              prompt_position = 'top',
+            },
+            file_ignore_patterns = { '.git/', 'vendor/', 'node_modules/' }
+        },
+        pickers = {
+            find_files = { hidden = true },     -- show hidden file
+            buffers = {
+                previewer = false,
+                layout_config = { width = 80 }
+            },
+        },
+      })
+      -- Enable telescope extensions, if they are installed
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>p', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+
+
+      -- Also possible to pass additional configuration options.
+      --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      vim.keymap.set('n', '<leader>s/', function()
+        builtin.live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end, { desc = '[S]earch [/] in Open Files' })
+
+    end,
+}
