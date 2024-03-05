ac66f65d2a6e9705bad11cb5862949ea70a703c9
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-03-05 21:24:51 +0300

    Add a misc config file

---
+++ nvim/lua/config/misc.lua
@@ -0,0 +1,9 @@
+-- this file just declares autocommands and stuff
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight yanked text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
