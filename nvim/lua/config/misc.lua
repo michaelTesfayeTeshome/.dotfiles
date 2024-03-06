e663d78e690f9dc09c9e16374f168e09b2f5e2a3
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-03-06 10:30:30 +0300

    Add formatter plugin

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -1,9 +1,16 @@
 -- this file just declares autocommands and stuff
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight yanked text',
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight yanked text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", { -- BufWritePre - right before saving the buffer to file
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
