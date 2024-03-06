4934b4b3a3a5d6e09cf93f8d30ce61b285cdd8df
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-03-06 10:34:54 +0300

    format

--- nvim/lua/michael/plugins/nvim-tree.lua
+++ nvim/lua/michael/plugins/nvim-tree.lua
@@ -1,25 +1,25 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
-    lazy = false,
-    opts = {
-        filters = { 
-            dotfiles = false
-        },
-        git = {
-            ignore = false
-        },
-        renderer = {
-            icons = {
-                show = {
-                    folder_arrow = false
-                }
-            },
-            indent_markers = {
-                enable = true
-            }
-        }
-    }
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	opts = {
+		filters = {
+			dotfiles = false,
+		},
+		git = {
+			ignore = false,
+		},
+		renderer = {
+			icons = {
+				show = {
+					folder_arrow = false,
+				},
+			},
+			indent_markers = {
+				enable = true,
+			},
+		},
+	},
 }
