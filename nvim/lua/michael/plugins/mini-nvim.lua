9f7052297d0ad131e9c15cc845d66250d22e0fc4
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-03-11 10:23:07 +0300

    Add text object surrounding

--- nvim/lua/michael/plugins/mini-nvim.lua
+++ nvim/lua/michael/plugins/mini-nvim.lua
@@ -12,6 +12,17 @@ return {
 			return "%2l:%-2v"
 		end
 
-        require("mini.pairs").setup()
+		require("mini.pairs").setup()
+		require("mini.surround").setup({
+			mappings = {
+				add = "<leader>sa", -- add surrounding
+				delete = "<leader>sd", -- Delete surrounding
+				find = "<leader>sf", -- Find surrounding (to the right)
+				find_left = "<leader>sF", -- Find surrounding (to the left)
+				highlight = "<leader>sh", -- Highlight surrounding
+				replace = "<leader>sr", -- Replace surrounding
+				update_n_lines = "<leader>sn", -- Update `n_lines`
+			},
+		})
 	end,
 }
