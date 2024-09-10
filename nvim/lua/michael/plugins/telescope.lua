b69fdf83ea9fa60dd9141ae2bdb595325cd305e5
Michael Tesfaye <michael.tesfaye.teshome@gmail.com>
2024-09-10 14:44:14 +0300

    add keymap for listing open buffers

--- nvim/lua/michael/plugins/telescope.lua
+++ nvim/lua/michael/plugins/telescope.lua
@@ -17,12 +17,14 @@ return {
 		{ "nvim-tree/nvim-web-devicons" },
 	},
 	config = function()
+		local actions = require("telescope.actions")
 		require("telescope").setup({
 			defaults = {
 				mappings = {
 					i = {
 						["<C-j>"] = "move_selection_next",
 						["<C-k>"] = "move_selection_previous",
+						["<C-d>"] = actions.delete_buffer + actions.move_to_top,
 						["<C-u>"] = false, -- clear out whats typed on the search prompt
 					},
 				},
@@ -48,7 +50,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[S]earch [F]iles" })
-		-- vim.keymap.set("n", "<leader>p", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>ls", builtin.buffers, { desc = "[ ] Find existing buffers" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 
